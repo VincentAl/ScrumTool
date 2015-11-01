76ca3180a6e471f1242492159f102031064026fa
tZiffel <thierry.ziffel@hei.fr>
2015-11-01 16:59:10 +0100

    Correction import annotation Entity

--- scrumtool-core/src/main/java/hei/gl/scrumtool/core/entity/Story.java
+++ scrumtool-core/src/main/java/hei/gl/scrumtool/core/entity/Story.java
@@ -3,31 +3,27 @@ package hei.gl.scrumtool.core.entity;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import org.hibernate.annotations.Entity;
-
-@SuppressWarnings("deprecation")
 @Entity
 public class Story {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
-	
+
 	private long numero;
-	
+
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "story")
 	private Set<Tache> listeTaches;
-	
+
 	private String titre;
 
-	
-//Getters-Setters
+	// Getters-Setters
 	public long getId() {
 		return id;
 	}
@@ -59,6 +55,5 @@ public class Story {
 	public void setTitre(String titre) {
 		this.titre = titre;
 	}
-	
-	
+
 }
--- scrumtool-core/src/main/java/hei/gl/scrumtool/core/entity/Tache.java
+++ scrumtool-core/src/main/java/hei/gl/scrumtool/core/entity/Tache.java
@@ -3,32 +3,29 @@ package hei.gl.scrumtool.core.entity;
 import java.sql.Time;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
-import org.hibernate.annotations.Entity;
-
-@SuppressWarnings("deprecation")
 @Entity
 public class Tache {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	private String titre;
-	
+
 	private String description;
-	
+
 	private String etat;
-	
+
 	private String priorite;
-	
+
 	private Time pointDepart;
-	
+
 	@ManyToOne(cascade = CascadeType.ALL)
 	private Story story;
 }
