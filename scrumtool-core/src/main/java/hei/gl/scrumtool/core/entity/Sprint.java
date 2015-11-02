807902d021c65ee96f766a10296be8e751dfe793
DAmaury <amaury.debruyckere@hei.fr>
2015-11-02 12:24:05 +0100

    modification des entiters et implemation des services

--- scrumtool-core/src/main/java/hei/gl/scrumtool/core/entity/Sprint.java
+++ scrumtool-core/src/main/java/hei/gl/scrumtool/core/entity/Sprint.java
@@ -32,4 +32,78 @@ public class Sprint {
 
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "sprint")
 	private Personne productOwner;
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Time getPointDepart() {
+		return pointDepart;
+	}
+
+	public void setPointDepart(Time pointDepart) {
+		this.pointDepart = pointDepart;
+	}
+
+	public Time getPointFin() {
+		return pointFin;
+	}
+
+	public void setPointFin(Time pointFin) {
+		this.pointFin = pointFin;
+	}
+
+	public long getNumero() {
+		return numero;
+	}
+
+	public void setNumero(long numero) {
+		this.numero = numero;
+	}
+
+	public List<Story> getListeStories() {
+		return listeStories;
+	}
+
+	public void setListeStories(List<Story> listeStories) {
+		this.listeStories = listeStories;
+	}
+	
+	public void addStory(Story story){
+		this.listeStories.add(story);
+	}
+	
+	public void addListStories(List<Story> stories){
+		this.listeStories.addAll(stories);
+	}
+	
+	public void removeStory(long idStory){
+		for (int i=0;i<this.listeStories.size();i++) {
+			if(this.listeStories.get(i).getId()==idStory){
+				this.listeStories.remove(i);
+				break;
+			}
+		}
+	}
+
+	public Personne getScrumMaster() {
+		return scrumMaster;
+	}
+
+	public void setScrumMaster(Personne scrumMaster) {
+		this.scrumMaster = scrumMaster;
+	}
+
+	public Personne getProductOwner() {
+		return productOwner;
+	}
+
+	public void setProductOwner(Personne productOwner) {
+		this.productOwner = productOwner;
+	}
 }
