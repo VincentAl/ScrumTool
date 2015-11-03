4b20ee305ee25e7dddbd2a458fb109d9c9c8738e
tZiffel <thierry.ziffel@hei.fr>
2015-11-03 17:50:25 +0100

    Delete Story ajax client side

--- scrumtool-web/src/main/java/hei/gl/scrumtool/web/controller/AjaxController.java
+++ scrumtool-web/src/main/java/hei/gl/scrumtool/web/controller/AjaxController.java
@@ -8,21 +8,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hei.gl.scrumtool.core.service.StoryService;
 
 @Controller
 public class AjaxController {
 	private final static Logger logger = LoggerFactory.getLogger(AjaxController.class);
-	
+
 	@Inject
 	private StoryService storyService;
-	
-	@RequestMapping(value="/story/{id}", method=RequestMethod.GET)
-	public String deleteOneStory(@PathVariable("id") long id){
+
+	@ResponseBody
+	@RequestMapping(value = "/story/{id}", method = RequestMethod.GET)
+	public String deleteOneStory(@PathVariable("id") long id) {
 		storyService.supprimer(id);
-		return "redirect:/";
+		return "";
 	}
-	
 
 }
--- scrumtool-web/src/main/webapp/resources/js/backlog.js
+++ scrumtool-web/src/main/webapp/resources/js/backlog.js
@@ -1,15 +1,25 @@
 dragula([ document.getElementById('1'), document.getElementById('2'),
 		document.getElementById('3') ])
 
-$(document)
-		.ready(
-				function() {
-					$("#toggleBtn")
-							.click(
-									function() {
-										$(".addForm").slideToggle();
-										$("#toggleBtn span")
-												.toggleClass(
-														"glyphicon-chevron-right glyphicon-chevron-down");
-									});
-				});
+$(document).ready(function() {
+	$("#toggleBtn").click(function() {
+		$(".addForm").slideToggle();
+		$("#toggleBtn span").toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
+		});
+	
+	$(".btnSup").click(function(){
+		var id = $(this).attr('id');
+		var $that=$(this).parent();
+		
+		$.ajax({
+			url : "story/"+id,
+			type : "GET",
+			
+			success : function(){
+				
+				$that.remove();
+			}
+		})
+		
+	});
+});
