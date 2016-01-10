21a9c7ee2814a29900a0a48ba0c1f0f26fc1741e
DAmaury <amaury.debruyckere@hei.fr>
2016-01-10 14:07:37 +0100

    remise en état de la fermeture d'un sprint

--- scrumtool-web/src/main/java/hei/gl/scrumtool/web/controller/SprintController.java
+++ scrumtool-web/src/main/java/hei/gl/scrumtool/web/controller/SprintController.java
@@ -135,6 +135,7 @@ private final static Logger logger = LoggerFactory.getLogger(BacklogController.c
 			((SprintServiceImpl) sprintService).setMessageHelper("The Sprint "+ numSprint +" is now closed, it can be found in the archive tab.");
 			((SprintServiceImpl) sprintService).setMessageHelperType("success");
 			
+			/*
 			Sprint sprint=sprintService.findCurrentSprint();
 			for(Story story : sprint.getStoryList()){
 				 if(story.getCategory()!=StoryColumn.DONE){
@@ -142,7 +143,7 @@ private final static Logger logger = LoggerFactory.getLogger(BacklogController.c
 					storyService.update(story);
 				 }
 			}
-			
+			*/
 			sprintService.closeCurrentSprint();
 		}
 		
